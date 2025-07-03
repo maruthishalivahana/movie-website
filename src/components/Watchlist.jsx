c0ad1a86a2d48d84384b81fe6e5b8bb5efb94595
maruthishalivahana <maruthikummari553@gmail.com>
2025-07-03 21:55:01 +0530

    started movie details page

--- src/components/Watchlist.jsx
+++ src/components/Watchlist.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import MovieDetailsCard from './MovieDetailsCard'
 
 function Watchlist() {
     return (
         <div>
-            hello from watchlist
+            <MovieDetailsCard />
 
         </div>
     )
