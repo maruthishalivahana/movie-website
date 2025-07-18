d44f9ef1fcf33263580973d7dbf5db74665165ea
maruthishalivahana <maruthikummari553@gmail.com>
2025-07-18 20:01:17 +0530

    sucessfully fetched recommendation data from the tmdb api

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,8 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router";
 import Movies from './components/Movies.jsx'
 import MovieDetailsCard from './components/MovieDetailsCard.jsx'
 import { WatchlistProvider } from '../WatchlistContext.jsx'
+import Recommendation from './components/Recommendation.jsx';
+
 function App() {
 
     return (
@@ -24,6 +26,7 @@ function App() {
                         <Route path='/moviedetails' element={<MovieDetailsCard />} />
                     </Routes>
                 </WatchlistProvider>
+                <Recommendation />
 
             </Router>
         </>
---
+++ src/components/Recommendation.jsx
@@ -0,0 +1,39 @@
+import React, { use } from 'react'
+import { useEffect } from 'react'
+import axios from 'axios'
+function Recommendation() {
+    // const movieId = 100;
+    const API_KEY = "913bb054a3e666b34c09d1f50d7c5139";
+    useEffect(() => {
+        try {
+            axios.get(`https://api.themoviedb.org/3/movie/100/recommendations?api_key=${API_KEY}&language=en-US&page=1`, {
+                // headers: {
+                //     accept: 'application/json',
+                //     Authorization: `Bearer ${API_KEY}`
+                // }
+            }).then((res) => {
+                const data = (res.data.results)
+                for (let i = 0; i <= data.length / 2; i++) {
+                    console.log(data[i])
+                    // this is the top 10 movies from the recommendatins
+                    // you can use this data to display the recommendations in your component
+
+                }
+            })
+
+        } catch (error) {
+            console.error("Error fetching recommendations:", error.message, error.response ? error.response.data : '');
+        }
+    }, [])
+
+    // Logic to fetch recommendations based on user's watchlist or preferences      
+
+
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Recommendation
